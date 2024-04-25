dfad46c844bd4c77f488c044474ebd9c4685ef07
AKUSAbeysinghe <154555496+AKUSAbeysinghe@users.noreply.github.com>
2024-04-26 01:43:43 +0530

    Add files via upload

---
+++ Casting/src/Animal.java
@@ -0,0 +1,23 @@
+class Animal {
+
+    public void eat(){
+        System.out.println("Omniverous diet!");
+    }
+}
+
+class Lion extends Animal {
+
+    public void eat() {
+        System.out.println("Carniverous diet!");
+    }
+
+}
+
+class Test{
+    public static void main(String[] args) {
+
+
+        Animal a = new Lion();
+        a.eat();
+    }
+}
